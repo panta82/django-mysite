a20768de3c3796ce8503af6f83a26c82f34b2033
Ivan Pantic <ivanpantic82@gmail.com>
2019-01-25 22:23:28 +0100

    Admin, editor

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.db import models
+from tinymce.widgets import TinyMCE
+
+from .models import Tutorial
+
+
+class TutorialAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Params', {
+			'fields': [
+				'tutorial_title',
+				'tutorial_published',
+			]
+		}),
+
+		('Content', {
+			'fields': [
+				'tutorial_content',
+			]
+		})
+	]
+
+	formfield_overrides = {
+		models.TextField: {
+			'widget': TinyMCE()
+		}
+	}
+
+
+admin.site.register(Tutorial, TutorialAdmin)
