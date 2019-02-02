eb61d212709184e9324a9ff2d88e04bc11b925a9
Ivan Pantic <ivanpantic82@gmail.com>
2019-02-02 23:12:50 +0100

    More models

--- main/admin.py
+++ main/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.db import models
 from tinymce.widgets import TinyMCE
 
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 
 
 class TutorialAdmin(admin.ModelAdmin):
@@ -12,6 +12,18 @@ class TutorialAdmin(admin.ModelAdmin):
 			'fields': [
 				'tutorial_title',
 				'tutorial_published',
+			]
+		}),
+
+		('URL', {
+			'fields': [
+				'tutorial_slug'
+			]
+		}),
+
+		('Series', {
+			'fields': [
+				'tutorial_series'
 			]
 		}),
 
@@ -29,4 +41,6 @@ class TutorialAdmin(admin.ModelAdmin):
 	}
 
 
+admin.site.register(TutorialCategory)
+admin.site.register(TutorialSeries)
 admin.site.register(Tutorial, TutorialAdmin)
--- main/models.py
+++ main/models.py
@@ -2,10 +2,35 @@ from django.db import models
 from datetime import datetime
 
 
+class TutorialCategory(models.Model):
+	tutorial_category = models.CharField(max_length=200)
+	category_summary = models.CharField(max_length=200)
+	category_slug = models.CharField(max_length=200)
+
+	class Meta:
+		verbose_name_plural = 'Categories'
+
+	def __str__(self):
+		return self.tutorial_category
+
+
+class TutorialSeries(models.Model):
+	tutorial_series = models.CharField(max_length=200)
+	tutorial_category = models.ForeignKey(TutorialCategory, verbose_name='Category',
+		null=True, on_delete=models.SET_NULL)
+	series_summary = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.tutorial_series
+
+
 class Tutorial(models.Model):
 	tutorial_title = models.CharField(max_length=200)
 	tutorial_content = models.TextField()
 	tutorial_published = models.DateTimeField('date published', default=datetime.now)
+	tutorial_series = models.ForeignKey(TutorialSeries, verbose_name='Series',
+		null=True, on_delete=models.SET_NULL)
+	tutorial_slug = models.CharField(max_length=200, null=True)
 
 	def __str__(self):
 		return self.tutorial_title
