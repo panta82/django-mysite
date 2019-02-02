2ef39e2fe16f197040f6a231c63a5cd849d44911
Ivan Pantic <ivanpantic82@gmail.com>
2019-02-02 21:56:41 +0100

    Made login / logout

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
 from main.models import Tutorial
 
@@ -8,3 +11,43 @@ def homepage(request: HttpRequest):
 	return render(request, 'main/home.html', {
 		'tutorials': Tutorial.objects.all()
 	})
+
+
+def register(request: HttpRequest):
+	if request.method == 'POST':
+		form = UserCreationForm(data=request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'New account created: {username}')
+			login(request, user)
+			return redirect('main:homepage')
+		else:
+			for msg in form.errors:
+				messages.error(request, f'{msg}: {form.errors[msg]}')
+
+	form = UserCreationForm
+	return render(request, 'main/register.html', {"form": form})
+
+
+def logout_request(request: HttpRequest):
+	logout(request)
+	messages.info(request, 'Logged out')
+	return redirect('main:homepage')
+
+
+def login_request(request: HttpRequest):
+	if request.method == 'POST':
+		form = AuthenticationForm(data=request.POST)
+		if form.is_valid():
+			user = form.get_user()
+			login(request, user)
+			messages.info(request, f'Welcome back, {user.username}!')
+			return redirect('main:homepage')
+
+		for key in form.errors:
+			for err in form.errors[key]:
+				messages.error(request, f'{key}: {err}' if key != '__all__' else err)
+
+	form = AuthenticationForm()
+	return render(request, 'main/login.html', {'form': form})
